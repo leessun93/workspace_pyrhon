e1823becbe2ef6cf4843530ce6317f74e8467309
leessun93 <dltjsgma@naver.com>
2022-05-16 19:31:49 +0900

    함수, if, for문 while문 자바랑 똑같네뭐 뭐 이정도쯤 개어렵네 ㅋㅋ

---
+++ for.py
@@ -0,0 +1,60 @@
+# for문
+
+#range() 어디부터~ 어디까지
+for waiting_no in range(1, 21): # == 1번부터 21미만까지
+    print('대기번호 :{0}번 입니다.' .format(waiting_no))
+
+#커피 완성 안내문
+starbucks = ["김갑순", "최칠복", "강명자", "이복자", "최동남"]
+for customer in starbucks :
+    print("{0}손님 커피 나오셨습니다." .format(customer))
+
+#while
+customer = '토르'
+index = 5
+while index >= 1 :
+    print("{0}, 커피가 준비되었습니다. {1}번 남았어요" .format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기처분 되었습니다.")
+
+# 조건이 맞을경우 while문 탈출
+#손님 이름이 김칠복일경우 while문을 멈추게하자
+customer = "김칠복"
+person = "Unknown"
+
+while person != customer :
+    print("{0}, 커피가 준비되었습니다." .format(customer))
+    person = input("이름이 어떻게 되세요? >")
+
+
+
+#continue == 계속해라
+absent = [2, 5, 11, 17]
+no_book = [3, 19]
+rude = [10]
+for student in range(1, 21) : #1~20
+    if student in absent :
+        continue #계속해서 진행하라
+    elif student in no_book :
+        print("{0}번은 교무실로 가서 손들고 서있어" .format(student))
+    elif student in rude :
+        print("{0}번 이런 어린x의 xx 너 따라와임마" .format(student))
+        break #멈춘다.
+    print("{0}아 계속해서 읽어봐!" .format(student))
+
+# 한줄 주석
+#출석번호가 12345일때 전부 100을 붙여서 101 102 103 등등으로 출력하라
+student = [1,2,3,4,5]
+student = [i + 100 for i in student] #배열에서 포문으로 값을 더했다(?)
+print(student)
+
+#학생의 이름을 길이로 변환 len
+student = ["ironman", "thor", "big_thor", "sall_thor"]
+student = [len(i) for i in student]
+print(student)
+
+#학생의 이름을 대문자로 변환 .apper
+student = ["ironman", "thor", "big_thor", "sall_thor"]
+student = [i.upper() for i in student]
+print(student)
---
+++ hamsu.py
@@ -0,0 +1,61 @@
+#함수
+#자바에서는 public int result(int i){ 
+#          return : 1;
+#          } 이렇게 사용하였다.
+
+def open_account():
+    print("새로운 계정이 생성되었습니다.")
+
+# 실행은
+open_account()
+
+#입 출금
+def deposit(balance, money): #입금
+    print("입금이 완료되었습니다. 잔액은 {0}원 입니다." .format(balance + money))
+    return balance + money
+
+def withdraw(balance, money): #출금
+    if balance >= money: #잔액이 출금금액보다 같거나 커야만 출력이되게한다
+        print("출금이 완료되었습니다. 잔액은 {0}원 입니다." .format(balance - money))
+        return balance - money
+    else : #잔액이 출금금액보다 작다면 출금을 막는다.
+        print("출금이 취소되었습니다. 잔액은 {0}원 입니다." .format(balance))
+        return balance
+
+#수수료
+def whitdraw_night(balance, money): #밤에 출금
+    commission = balance*0.01 #수수료는 예금의 1프로
+    return commission, balance - money - commission #수수료와 잔액값 이렇게 여러 값을 리턴 할 수 있다.
+
+
+
+
+balance = 0
+balance = deposit(balance, 10000)
+print(balance)
+
+balance = withdraw(balance, 200)
+print(balance)
+
+commission, balance = whitdraw_night(balance, 5000)
+print("수수료 {0}원 이며, 잔액은 {1}원 남았습니다. " .format(commission, balance))
+
+
+#함수 호출
+def profile(name, age, lang):
+    print("이름은 {0} 나이는 {1} 언어는 {2}" .format(name, age, lang))
+
+profile("박명수", 30 , "java") #순서대로 값을 넣어주면 실행되고
+profile(age=40, name="유재석", lang="pythone") #순서가 바뀌어도 변수를 잘 맞춰주면 실행된다.
+
+
+#가변인자
+#갯수가 정해지지않은 값이 있을때 유동적으로 갯수를 넣어야할때
+def profile(name, age, *lang):
+    print("이름은 {0} 이고 나이는{1} 입니다." .format(name, age), end=" ")   #ende(" ") 는 줄바꿈을 하지않고 이어서 붙이겠다는뜻이다
+    for language in lang:
+        print(language, end=" ")
+    print()
+
+profile("아이언맨", 50, "java","python", "c++", "c", "javascript", "jQuery", "SQL")
+profile("터미네이터", 69, "english")
